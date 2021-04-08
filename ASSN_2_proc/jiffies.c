80f877e9586d73ec8253b01e85be417320fc1555
Joel Marceline <53477893+Joel-Marc@users.noreply.github.com>
2021-04-08 13:15:46 +0530

    Update jiffies.c

--- ASSN_2_proc/jiffies.c
+++ ASSN_2_proc/jiffies.c
@@ -10,47 +10,48 @@
 
 
 ssize_t proc_read(struct file *file, char __user *usr_buf,size_t count, loff_t *pos);
+
 static struct file_operations proc_ops ={
-.owner = THIS_MODULE,
-.read = proc_read,
+  .owner = THIS_MODULE,
+  .read = proc_read,
 };
 
 /* This function is called when the module is loaded. */
 int proc_init(void){
 
 /* creates the /proc/hello entry */
-proc_create(PROC_NAME, 0666, NULL, &proc_ops);
-return 0;
+  proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+  return 0;
 }
 
 /* This function is called when the module is removed. */
 void proc_exit(void){
 
 /* removes the /proc/hello entry */
-remove_proc_entry(PROC_NAME, NULL);
+  remove_proc_entry(PROC_NAME, NULL);
 }
 
 /* This function is called each time /proc/hello is read */
 ssize_t proc_read(struct file *file, char  __user *usr_buf,size_t count, loff_t *pos)
 {
-int rv = 0;
-char buffer[BUFFER_SIZE];
-static int completed = 0;
-if (completed)
-{
-completed = 0;
-return 0;
+  int rv = 0;
+  char buffer[BUFFER_SIZE];
+  static int completed = 0;
+
+  if (completed)
+  {
+  completed = 0;
+  return 0;
+  }
+  completed = 1;
+  rv = sprintf(buffer, "JIFFIES : %lu \n",jiffies);
+  /* copies kernel space buffer to user space usrbuf */
+  raw_copy_to_user(usr_buf, buffer, rv);
+  return rv;
 }
-completed = 1;
-rv = sprintf(buffer, "JIFFIES : %lu \n",jiffies);
-//rv = sprintf(buffer , "HELLO WORLD!!!");
-/* copies kernel space buffer to user space usrbuf */
-raw_copy_to_user(usr_buf, buffer, rv);
-return rv;
-}
+
 module_init(proc_init);
 module_exit(proc_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Hello Module");
 MODULE_AUTHOR("SGG");
-
