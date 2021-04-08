93a5e08e47a445f63a07b9faf74eafb95c5bd8db
Joel Marceline <53477893+Joel-Marc@users.noreply.github.com>
2021-04-08 09:09:45 +0530

    indentation

--- ASSN_1_kern/1.c
+++ ASSN_1_kern/1.c
@@ -5,7 +5,8 @@
 #include <linux/gcd.h>
 #include <asm/param.h>
 
-/* This function is called when the module is loaded. */ int simple_init(void)
+/* This function is called when the module is loaded. */ 
+int simple_init(void)
 {
 printk(KERN_INFO "Loading Module\n");
 printk(KERN_INFO "GOLDEN RATIO : %llu\n", GOLDEN_RATIO_PRIME);
@@ -13,7 +14,8 @@ printk(KERN_INFO "JIFFIES : %ld\n",jiffies);
 return 0;
 
 }
-/* This function is called when the module is removed. */ void simple_exit(void)
+/* This function is called when the module is removed. */ 
+void simple_exit(void)
 {
 printk(KERN_INFO "Removing Module\n");
 printk(KERN_INFO "GCD OF 3300 and 24  IS : %ld\n",gcd(3300,24));
