d86f46a13886e0272fd8172f8b44ad13429a1026
Joel Marceline <53477893+Joel-Marc@users.noreply.github.com>
2021-04-08 13:16:47 +0530

    Update seconds.c

--- ASSN_2_proc/seconds.c
+++ ASSN_2_proc/seconds.c
@@ -11,48 +11,47 @@
 unsigned long js, je ,tet;
 ssize_t proc_read(struct file *file, char __user *usr_buf,size_t count, loff_t *pos);
 static struct file_operations proc_ops ={
-.owner = THIS_MODULE,
-.read = proc_read,
+  .owner = THIS_MODULE,
+  .read = proc_read,
 };
 
 /* This function is called when the module is loaded. */
 int proc_init(void){
-js=jiffies;
+  js=jiffies;
 /* creates the /proc/hello entry */
-proc_create(PROC_NAME, 0666, NULL, &proc_ops);
-return 0;
+  proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+  return 0;
 }
 
 /* This function is called when the module is removed. */
 void proc_exit(void){
 
 /* removes the /proc/hello entry */
-remove_proc_entry(PROC_NAME, NULL);
+  remove_proc_entry(PROC_NAME, NULL);
 }
 
 /* This function is called each time /proc/hello is read */
 ssize_t proc_read(struct file *file, char  __user *usr_buf,size_t count, loff_t *pos)
 {
-int rv = 0;
-char buffer[BUFFER_SIZE];
-static int completed = 0;
-if (completed)
-{
-completed = 0;
-return 0;
-}
-completed = 1;
-je=jiffies;
-tet=je-js;
-rv = sprintf(buffer, "TIME ELAPSED : %lu \n",tet/HZ);
-//rv = sprintf(buffer , "HELLO WORLD!!!");
+  int rv = 0;
+  char buffer[BUFFER_SIZE];
+  static int completed = 0;
+  if (completed)
+  {
+  completed = 0;
+  return 0;
+  }
+  completed = 1;
+  je=jiffies;
+  tet=je-js;
+  rv = sprintf(buffer, "TIME ELAPSED : %lu \n",tet/HZ);
+
 /* copies kernel space buffer to user space usrbuf */
-raw_copy_to_user(usr_buf, buffer, rv);
-return rv;
+  raw_copy_to_user(usr_buf, buffer, rv);
+  return rv;
 }
 module_init(proc_init);
 module_exit(proc_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Hello Module");
 MODULE_AUTHOR("SGG");
-
